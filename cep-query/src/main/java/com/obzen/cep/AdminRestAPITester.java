4d461c8167bcb2933d599e4c3d4460824fd3d994
Ernest HJ Lee <hunjae.lee@gmail.com>
2016-01-18 19:49:33 +0900

    Revise cep-query and add defailed explanation

--- cep-query/src/main/java/com/obzen/cep/AdminRestAPITester.java
+++ cep-query/src/main/java/com/obzen/cep/AdminRestAPITester.java
@@ -29,14 +29,6 @@ import java.nio.file.Path;
 
 public class AdminRestAPITester {
 
-    //private static final String baseUrl = "http://192.168.10.83:8082/rest/nodes/";
-    //private static final String nodeId = "node.192.168.10.83.5800";
-    ////private static final String baseUrl = "http://localhost:8082/rest/nodes/";
-    ////private static final String nodeId = "node.localhost.5800";
-    ////private static final String baseUrl = "http://192.168.10.83:8082/rest/nodes/";
-    ////private static final String nodeId = "node.192.168.10.83.5800";
-    //private static final String targetUrl = baseUrl + nodeId + "/services";
-
     public static void main(String... args) throws Exception {
         //Parse arguments
         OptionParser parser = new OptionParser();
